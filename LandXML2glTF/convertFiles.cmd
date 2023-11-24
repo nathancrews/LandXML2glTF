d64f9ba9db01f1b899a20b3436af4579a1ae3f94
nathancrews <nathan@nathancrews.com>
2023-11-24 17:24:38 +0200

    updating batch file

--- LandXML2glTF/convertFiles.cmd
+++ LandXML2glTF/convertFiles.cmd
@@ -1,3 +1,6 @@
 rem converts all files in LandXML example directory and sub-directories to .gltf
 cd .\LXML2glTF\bin\x64\Release\
-.\LXML2glTF.exe "E:\LandXML\Uploaded Files\*" /s
+rem .\LXML2glTF.exe "E:\LandXML\Uploaded Files\*" /s
+rem .\LXML2glTF.exe "E:\LandXML\Sample Files\*" /s
+rem .\LXML2glTF.exe "E:\LandXML\Schema Sample Files\*" /s
+.\LXML2glTF.exe "E:\adeskProjectData\*" /s

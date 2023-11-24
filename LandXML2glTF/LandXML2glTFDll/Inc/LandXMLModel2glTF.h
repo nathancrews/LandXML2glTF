e568a1d07392599b7047ccdf3896e8c86ec53074
nathancrews <nathan@nathancrews.com>
2023-11-24 10:04:25 +0200

    refactored GLTF classes

--- LandXML2glTF/LandXML2glTFDll/Inc/LandXMLModel2glTF.h
+++ LandXML2glTF/LandXML2glTFDll/Inc/LandXMLModel2glTF.h
@@ -2,9 +2,8 @@
 
 #include "framework.h"
 #include "Models/LandXML.h"
-#include "Writers/GLTFWriter.h"
+#include "Models/GLTFModel.h"
 #include "Helpers/MathHelper.h"
-#include <IStreamWriter.h>
 #include "tinyxml2.h"
 
 #ifdef _MSC_VER
@@ -17,67 +16,6 @@ class OGRSpatialReference;
 
 namespace LANDXML2GLTF
 {
-
-struct GLTFSurfaceMaterial
-{
-    GLTFSurfaceMaterial() 
-    {
-        m_material.id = "0";
-        m_material.metallicRoughness.baseColorFactor = Microsoft::glTF::Color4(0.0f, 1.0f, 0.0f, 1.0f);
-        m_material.metallicRoughness.metallicFactor = 0.2f;
-        m_material.metallicRoughness.roughnessFactor = 0.4f;
-        m_material.doubleSided = true;
-    }
-
-    Microsoft::glTF::Color4 m_color = Microsoft::glTF::Color4(0.0f, 1.0f, 0.0f, 1.0f);
-    Microsoft::glTF::Material m_material;
-};
-
-struct GLTFSurfaceModel
-{
-    std::string name;
-
-    std::vector<float> gltfMeshPoints;
-    std::vector<std::string> accessorIdPositions;
-    std::vector<std::string> accessorIdIndices;
-
-    std::unordered_map<unsigned int, unsigned int> gltfSubMeshIndicesMaterialMap;
-    std::unordered_map<unsigned int, std::vector<unsigned int>> gltfSubMeshIndexIDs;
-};
-
-struct GLTFPolylineModel
-{
-    std::string name;
-    unsigned int m_materialId;
-
-    std::vector<float> gltfPolylinePoints;
-    std::vector<std::string> accessorIdPolylinePositions;
-    std::vector<std::string> accessorIdPolylineIndices;
-    std::vector<unsigned int> gltfPolylineIndexIDs;
-};
-
-struct GLTFModel
-{
-    ~GLTFModel()
-    {
-        for (GLTFSurfaceModel* gltfSurfModel : gltfSurfaceModels)
-        {
-            delete gltfSurfModel;
-        }
-
-        for (GLTFPolylineModel* gltfPolyModel : gltfPolylineModels)
-        {
-            delete gltfPolyModel;
-        }
-    }
-
-    std::string name;
-
-    std::vector<GLTFSurfaceMaterial> gltfMeshMaterials;
-    std::vector<GLTFSurfaceModel*> gltfSurfaceModels;
-    std::vector<GLTFPolylineModel*> gltfPolylineModels;
-};
-
 
 class LANDXML2GLTFDLLAPI LandXMLModel2glTF
 {
---
+++ LandXML2glTF/LandXML2glTFDll/Inc/Models/GLTFModel.h
@@ -0,0 +1,69 @@
+#pragma once
+#include "framework.h"
+#include <IStreamWriter.h>
+#include "Writers/GLTFWriter.h"
+
+namespace LANDXML2GLTF
+{
+
+struct GLTFSurfaceMaterial
+{
+    GLTFSurfaceMaterial()
+    {
+        m_material.id = "0";
+        m_material.metallicRoughness.baseColorFactor = Microsoft::glTF::Color4(0.0f, 1.0f, 0.0f, 1.0f);
+        m_material.metallicRoughness.metallicFactor = 0.2f;
+        m_material.metallicRoughness.roughnessFactor = 0.4f;
+        m_material.doubleSided = true;
+    }
+
+    Microsoft::glTF::Color4 m_color = Microsoft::glTF::Color4(0.0f, 1.0f, 0.0f, 1.0f);
+    Microsoft::glTF::Material m_material;
+};
+
+struct GLTFSurfaceModel
+{
+    std::string name;
+
+    std::vector<float> gltfMeshPoints;
+    std::vector<std::string> accessorIdPositions;
+    std::vector<std::string> accessorIdIndices;
+
+    std::unordered_map<unsigned int, unsigned int> gltfSubMeshIndicesMaterialMap;
+    std::unordered_map<unsigned int, std::vector<unsigned int>> gltfSubMeshIndexIDs;
+};
+
+struct GLTFPolylineModel
+{
+    std::string name;
+    unsigned int m_materialId;
+
+    std::vector<float> gltfPolylinePoints;
+    std::vector<std::string> accessorIdPolylinePositions;
+    std::vector<std::string> accessorIdPolylineIndices;
+    std::vector<unsigned int> gltfPolylineIndexIDs;
+};
+
+struct GLTFModel
+{
+    ~GLTFModel()
+    {
+        for (GLTFSurfaceModel* gltfSurfModel : gltfSurfaceModels)
+        {
+            delete gltfSurfModel;
+        }
+
+        for (GLTFPolylineModel* gltfPolyModel : gltfPolylineModels)
+        {
+            delete gltfPolyModel;
+        }
+    }
+
+    std::string name;
+
+    std::vector<GLTFSurfaceMaterial> gltfMeshMaterials;
+    std::vector<GLTFSurfaceModel*> gltfSurfaceModels;
+    std::vector<GLTFPolylineModel*> gltfPolylineModels;
+};
+
+}

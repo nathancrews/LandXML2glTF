93a0e352d268077214b27b2ce29c5cf0b20bcbac
nathancrews <149575716+nathancrews@users.noreply.github.com>
2023-11-29 05:06:55 +0200

    Update convertFiles.cmd
    Removed extended test commands

--- LandXML2glTF/convertFiles.cmd
+++ LandXML2glTF/convertFiles.cmd
@@ -2,8 +2,3 @@ rem converts all files in LandXML example directory and sub-directories to .gltf
 cd .\LXML2glTF\bin\x64\Release\
 .\LXML2glTF.exe "../../../../LandXML/*" /s
 
-rem for testing 2k+ sample LandXML files
-rem .\LXML2glTF.exe "E:\LandXML\Uploaded Files\*" /s
-rem .\LXML2glTF.exe "E:\LandXML\Sample Files\*" /s
-rem .\LXML2glTF.exe "E:\LandXML\Schema Sample Files\*" /s
-rem .\LXML2glTF.exe "E:\adeskProjectData\*" /s

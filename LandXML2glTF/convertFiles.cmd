6524c21e8b0d54cf2f534c98b0bb4c92bb366ac1
nathancrews <nathan@nathancrews.com>
2023-11-30 09:38:16 +0200

    updated script to use new exe output file location

--- LandXML2glTF/convertFiles.cmd
+++ LandXML2glTF/convertFiles.cmd
@@ -1,4 +1,4 @@
 rem converts all files in LandXML example directory and sub-directories to .gltf
-cd .\LXML2glTF\bin\x64\Release\
-.\LXML2glTF.exe "../../../../LandXML/*" /s
+cd build\debug
+.\LXML2glTF.exe "../../LandXML/*" /s
 
